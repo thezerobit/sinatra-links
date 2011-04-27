d2a0f77955f8ae54b498be814820daadd69b1e09
Stephen A. Goss <steveth45@gmail.com>
2011-04-26 21:43:03 -0700

    made address length 255

--- lib/link.rb
+++ lib/link.rb
@@ -5,7 +5,7 @@ class Link
 
   property :id,         Serial
   property :name,       String, :required => true, :unique_index => true
-  property :address,    String, :required => true, :unique_index => true
+  property :address,    String, :required => true, :unique_index => true, :length => 255
   property :votes,      Integer, :required => true, :default => 0
   property :created_at, DateTime
   property :updated_at, DateTime
